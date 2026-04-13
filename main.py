4c85769d67e7db89930afe581b57b3e734455ce2
LeoSoaresK <leosojunior@gmail.com>
2026-04-13 10:37:29 -0300

    Classe TestResult

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ class TestStub(TestCase):
         assert False
 
     def test_error(self):
-        raise Exception
+        raise Exception("Erro inesperado")
 
 if __name__ == "__main__":
     result = TestResult()
     
-    test1 = TestStub("test_success")
-    test1.run(result)
+    test_pass = TestStub("test_success")
+    test_pass.run(result)
     
-    test2 = TestStub("test_failure")
-    test2.run(result)
+    test_fail = TestStub("test_failure")
+    test_fail.run(result)
     
-    test3 = TestStub("test_error")
-    test3.run(result)
+    test_err = TestStub("test_error")
+    test_err.run(result)
     
-    print(result.summary())
+    print(f"Relatório Final: {result.summary()}")
     assert result.summary() == "3 run, 1 failed, 1 error"
