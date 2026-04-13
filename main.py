a3ed63e25882aa8db5c934c1547eb450923fe128
LeoSoaresK <leosojunior@gmail.com>
2026-04-13 10:49:24 -0300

    Testando TestCase

--- main.py
+++ main.py
@@ -8,19 +8,59 @@ class TestStub(TestCase):
         assert False
 
     def test_error(self):
-        raise Exception("Erro inesperado")
+        raise Exception
+
+class TestSpy(TestCase):
+    def __init__(self, name):
+        super().__init__(name)
+        self.log = ""
+
+    def set_up(self):
+        self.log += "set_up "
+
+    def test_method(self):
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.log += "tear_down"
+
+class TestCaseTest(TestCase):
+    def set_up(self):
+        self.result = TestResult()
+
+    def test_result_success_run(self):
+        stub = TestStub("test_success")
+        stub.run(self.result)
+        assert self.result.summary() == "1 run, 0 failed, 0 error"
+
+    def test_result_failure_run(self):
+        stub = TestStub("test_failure")
+        stub.run(self.result)
+        assert self.result.summary() == "1 run, 1 failed, 0 error"
+
+    def test_result_error_run(self):
+        stub = TestStub("test_error")
+        stub.run(self.result)
+        assert self.result.summary() == "1 run, 0 failed, 1 error"
+
+    def test_template_method(self):
+        spy = TestSpy("test_method")
+        spy.run(self.result)
+        assert spy.log == "set_up test_method tear_down"
 
 if __name__ == "__main__":
-    result = TestResult()
+    final_result = TestResult()
     
-    test_pass = TestStub("test_success")
-    test_pass.run(result)
+    # Lista de testes para validar o framework
+    tests = [
+        TestCaseTest("test_result_success_run"),
+        TestCaseTest("test_result_failure_run"),
+        TestCaseTest("test_result_error_run"),
+        TestCaseTest("test_template_method")
+    ]
     
-    test_fail = TestStub("test_failure")
-    test_fail.run(result)
+    for test in tests:
+        test.run(final_result)
     
-    test_err = TestStub("test_error")
-    test_err.run(result)
-    
-    print(f"Relatório Final: {result.summary()}")
-    assert result.summary() == "3 run, 1 failed, 1 error"
+    print(f"{final_result.summary()}")
+    assert final_result.failures == [] and final_result.errors == []
