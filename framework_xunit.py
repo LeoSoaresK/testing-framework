ed1a5503d4ef1da633a7c372aface28154a22719
LeoSoaresK <leosojunior@gmail.com>
2026-04-13 11:05:14 -0300

    Comandos Assert

--- framework_xunit.py
+++ framework_xunit.py
@@ -62,7 +62,7 @@ class TestCase:
             test_method()
         except AssertionError:
             result.add_failure(self.test_method_name)
-        except Exception as e:
+        except Exception:
             result.add_error(self.test_method_name)
         self.tear_down()
 
@@ -71,3 +71,19 @@ class TestCase:
 
     def tear_down(self):
         pass
+
+    def assert_equal(self, first, second):
+        if first != second:
+            raise AssertionError(f"{first} != {second}")
+
+    def assert_true(self, expr):
+        if not expr:
+            raise AssertionError(f"{expr} is not true")
+
+    def assert_false(self, expr):
+        if expr:
+            raise AssertionError(f"{expr} is not false")
+
+    def assert_in(self, member, container):
+        if member not in container:
+            raise AssertionError(f"{member} not found in {container}")
